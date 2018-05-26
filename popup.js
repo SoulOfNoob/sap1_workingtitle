b6529c23a52a7c1fc7bc9f3edd8a3ad9fe57b3ab
Jan Ryklikas <jry@facelift-bbt.com>
2018-05-27 00:36:01 +0200

    light corrections

--- popup.js
+++ popup.js
@@ -6,6 +6,8 @@ $(function () {
         location_data = result.location_data;
         console.log(location_data);
         var emission_sum = 0;
+        var emission_mode_array = [];
+        var emission_distance_array = [];
         $('#emissions').append($('<tr><th scope="col">Uhrzeit</th><th scope="col">Verkehrsmittel</th><th scope="col">Entfernung</th><th scope="col">Emissionen</th></tr>'));
         $(location_data).each(function () {
             var row = $('<tr></tr>');
@@ -25,51 +27,61 @@ $(function () {
             }
             var emissions = $('<td style="color: ' + font_color + '"></td>').html(emission_val + "kg");
             emission_sum += emission_val;
+            emission_mode_array.push(this.type);
+            emission_distance_array.push(this.distance);
 
             console.log(this.time, this.type, this.distance);
             console.log(row, time, type, distance);
 
             $('#emissions').append($(row).append($(time)).append($(type)).append($(distance)).append($(emissions)));
         });
-        $('#emissions').append($('<tr><th colspan="3" >Gesamt: </th><th>' + emission_sum + '</th></tr>'));
+        $('#emissions').append($('<tr><th colspan="3" >Gesamt: </th><th>' + emission_sum + '(' + calculatePoints(emission_mode_array, emission_distance_array) + ')</th></tr>'));
     });
 
 });
+var emissions = [];
+emissions[2] = 0;
+emissions[29] = 140;
+emissions[36] = 140;
+emissions[7] = 60;
+emissions[3] = 0;
+emissions[9] = 65;
+emissions[8] = 50;
+emissions[5] = 214;
+emissions[10] = 65;
+emissions[30] = 140; //Motorrad
+//11,31,34,13
+emissions[12] = 65;
+//24,15,16
+emissions[6] = 0;
+emissions[32] = 0;
+emissions[37] = 0;
+emissions[35] = 0;
+emissions[33] = 0;
+emissions[17] = 0;
+emissions[22] = 0;
+emissions[25] = 0;
+emissions[27] = 0;
+emissions[18] = 0;
+emissions[21] = 0;
+emissions[19] = 0;
+emissions[20] = 0;
+emissions[23] = 0;
+emissions[26] = 0;
+emissions[14] = 0;
+emissions[0] = 0;
 
 function getEmissions(mode_id, distance) {
-    var emissions = [];
-    emissions[2] = 0;
-    emissions[29] = 140;
-    emissions[36] = 140;
-    emissions[7] = 60;
-    emissions[3] = 0;
-    emissions[9] = 65;
-    emissions[8] = 50;
-    emissions[5] = 214;
-    emissions[10] = 65;
-    emissions[30] = 140; //Motorrad
-    //11,31,34,13
-    emissions[12] = 65;
-    //24,15,16
-    emissions[6] = 0;
-    emissions[32] = 0;
-    emissions[37] = 0;
-    emissions[35] = 0;
-    emissions[33] = 0;
-    emissions[17] = 0;
-    emissions[22] = 0;
-    emissions[25] = 0;
-    emissions[27] = 0;
-    emissions[18] = 0;
-    emissions[21] = 0;
-    emissions[19] = 0;
-    emissions[20] = 0;
-    emissions[23] = 0;
-    emissions[26] = 0;
-    emissions[14] = 0;
-    emissions[0] = 0;
-
     return emissions[mode_id] * distance / 1000000;
+}
+
+function calculatePoints(modes, distances) {
+    var points = 0;
+    for (var i = 0; i < modes.length; i++) {
+        points += (140 - emissions[modes[i]]) * distances[i];
+        points -= emissions[modes[i]] * distances[i];
+    }
+    return points + 1400;
 }
 
 function getTypeName(type_id) {

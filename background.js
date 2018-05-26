d6976800ca28ed0cb24e5e87b77116753ba318b7
Jan Ryklikas <jry@facelift-bbt.com>
2018-05-26 20:39:21 +0200

    Crawling Works

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.runtime.onInstalled.addListener(function () {
     chrome.webNavigation.onCompleted.addListener(function (details) {
+        // chrome.tabs.getSelected(null, function(tab){
+        //     chrome.tabs.executeScript(tab.id, {file: "start.js"});
+        // });
         var newURL = "http://www.google.de/maps/timeline?pb=!1m2!1m1!1s2018-05-25";
         chrome.tabs.create({ url: newURL });
     },
@@ -12,13 +15,13 @@ chrome.runtime.onInstalled.addListener(function () {
 
     chrome.webNavigation.onCompleted.addListener(function (details) {
         chrome.tabs.getSelected(null, function(tab){
-            chrome.tabs.executeScript(tab.id, {code: "alert('test');"});
+            chrome.tabs.executeScript(tab.id, {file: "crawler.js"});
         });
     },
     {
         url: [{
             // Runs on example.com, example.net, but also example.foo.com
-            hostContains: 'google'
+            pathContains: 'timeline'
         }]
     });
 });
--- crawler.js
+++ crawler.js
@@ -1,16 +1,14 @@
 console.log( "loaded!" );
-alert("IT WORKS");
-// $(function() {
-//     console.log( "ready!" );
-//
-//     $('.activities-wrapper').each(function(){
-//         //console.log($(this));
-//         var type = $(this).find('.activity-type').text();
-//         var distance = $(this).find('.activity-type').parent().attr('data-distance');
-//         var time = $(this).parent().parent().prev().find('.segment-duration-part').last().text();
-//
-//         console.log('type: ', type);
-//         console.log('distance: ', distance);
-//         console.log('time: ', time);
-//     });
-// });
+$(function() {
+    console.log( "jQ ready!" );
+    $('.activities-wrapper').each(function(){
+        //console.log($(this));
+        var type = $(this).find('.activity-type').text();
+        var distance = $(this).find('.activity-type').parent().attr('data-distance');
+        var time = $(this).parent().parent().prev().find('.segment-duration-part').last().text();
+
+        console.log('type: ', type);
+        console.log('distance: ', distance);
+        console.log('time: ', time);
+    });
+});
